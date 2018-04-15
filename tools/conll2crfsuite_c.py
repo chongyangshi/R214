f8645ad4eeb113bb3297e32c74cca244b0e132ff
icydoge <icydoge@gmail.com>
2018-04-15 02:40:46 +0100

    Adjust script to generate single feature file.

--- tools/conll2crfsuite_c.py
+++ tools/conll2crfsuite_c.py
@@ -9,12 +9,10 @@ import sys
 from os import path, getcwd
 from collections import OrderedDict
 
-if len(sys.argv) < 4:
-    raise RuntimeError("Need to supply paths to train, devel, and test CSVs.")
+if len(sys.argv) < 2:
+    raise RuntimeError("Need to supply path to input TSV.")
 
-train_csv = open(path.abspath(path.expanduser(sys.argv[1])), 'r')
-devel_csv = open(path.abspath(path.expanduser(sys.argv[2])), 'r')
-test_csv = open(path.abspath(path.expanduser(sys.argv[3])), 'r')
+train_tsv = open(path.abspath(path.expanduser(sys.argv[1])), 'r')
 trigram_path = path.join(path.dirname(path.dirname(path.realpath(__file__))), 'pubmed')
 
 trigrams = ["lemma", "pos", "soundex"]
@@ -46,13 +44,11 @@ print("Using features: {}.".format(str(attribute_templates)))
 
 feature_extractor = lambda x: crfutils.apply_templates(x, attribute_templates)
 
-for fi, txt in [(train_csv, "train"), (devel_csv, "devel"), (test_csv, "test")]:
+for fi, txt in [(train_tsv, "train")]:
     write_to = path.join(trigram_path, txt + "_trigrams.crfsuite")
     fo = open(write_to, "w+")
     print("Writing to {}...".format(write_to))
     crfutils.main(feature_extractor, fi, fo, fields=input_columns, sep='\t')
     fo.close()
 
-train_csv.close()
-devel_csv.close()
-test_csv.close()
+train_tsv.close()
